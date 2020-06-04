615626bbee16e3f241bf4e1f2bf09d3a12340f6f
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:59:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Figgle;
+using Humanizer;
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digíte seu nome: ");
+            string nome=Console.ReadLine();
+            string arte=FiggleFonts.Slant.Render(nome);
+            Console.WriteLine(arte);
+            
+        }
+    }
+}
